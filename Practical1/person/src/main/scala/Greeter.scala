b8c6420c468ee053060466ff0bdab30b97df8123
Aswathy Rajeev <92585212+21suny@users.noreply.github.com>
2022-09-19 11:19:37 +0100

    Delete Practical1/person directory

--- Practical1/person/src/main/scala/Greeter.scala
+++
@@ -1,6 +0,0 @@
-class Greeter(val name: String):
-def hi: String =
-  "hi " + name
-
-def bye: String =
-  "bye " + name
--- Practical1/person/src/main/scala/Main.scala
+++
@@ -1,5 +0,0 @@
-@main def main: Unit =
-val myGreeter = new Greeter("John")
-for (i <- 1 to 3)
-  println(myGreeter.hi)
-  println(myGreeter.bye)
